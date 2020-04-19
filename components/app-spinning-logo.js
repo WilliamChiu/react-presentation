914c619e00685f0ad2fa497e8d73ca6f6b9da6a0
Kevin Li <kevinl94303spam@gmail.com>
2020-04-19 00:09:12 -0700

    app spinning logo

---
+++ components/app-spinning-logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app-spinning-logo.css'
+
+function ReactLogo(props) {
+    return (<div className="applogo-container">
+        <img className="applogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"/>
+    </div>)
+}
+
+export default ReactLogo;
