179e4217f1bc65b990f074007a075491f69a2b38
Kevin Li <kevinl94303spam@gmail.com>
2020-04-18 18:07:32 -0700

    added theme, static folder

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+    fonts: {
+      body: 'Helvetica, sans-serif',
+      monospace: '"Roboto Mono", monospace',
+    },
+    colors: {
+      text: 'white',
+      background: 'black',
+      primary: 'blue',
+    },
+  }
