faa8f7ad512a40f6f2c3ab300819daa8f1da83d0
William Chiu <williamchiu16@gmail.com>
2020-04-22 05:31:18 -0400

    remove 40vw

--- components/prop-state-container.js
+++ components/prop-state-container.js
@@ -3,9 +3,7 @@ import styled from "styled-components"
 const StatesPropsPreContainer = styled.div`
     margin-top: -55px;
     font-size: 1rem;
-    div {
-        width: 40vw;
-    }
+
     pre {
         font-size: 0.8rem;
     }
