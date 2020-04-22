1347a6d4c1be6e2ffcaa8d47726811f11fdecbc1
William Chiu <williamchiu16@gmail.com>
2020-04-22 15:38:45 -0400

    done

--- components/smaller.js
+++ components/smaller.js
@@ -4,6 +4,24 @@ let Smaller = styled.div`
   pre {
     font-size: ${props => props.size || 1}rem;
   }
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 0;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+
+  ul {
+    margin-top: 0;
+  }
+
+  a {
+    font-size: 1.5rem;
+    color: gray;
+  }
 `
 
 export default Smaller
